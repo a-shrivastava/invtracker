003415e4c8896e7bd11d2db48d49f902fb01dca9
abhishek <abhishek@192.168.8.107>
2016-01-24 00:29:06 -0500

    csv read attempt 2

--- src/main/java/invtracker/TickerAdder.java
+++ src/main/java/invtracker/TickerAdder.java
@@ -11,8 +11,6 @@ import javax.servlet.http.HttpServletResponse;
 import javax.sql.*;
 import javax.naming.*;
 
-import com.opencsv.CSVReader;
-
 import java.io.PrintWriter;
 import java.net.URL;
 import java.sql.*;
@@ -31,20 +29,13 @@ public class TickerAdder extends HttpServlet {
 					"http://real-chart.finance.yahoo.com/table.csv?s=GOOG&a=00&b=01&c=2016&d=00&e=24&f=2016&g=d&ignore=.csv");
 			BufferedReader in = new BufferedReader(new InputStreamReader(
 					stockURL.openStream()));
-			CSVReader reader = new CSVReader(in);
+//			CSVReader reader = new CSVReader(in);
 
-			String[] nextLine;
-			while ((nextLine = reader.readNext()) != null) {
-				// nextLine[] is an array of values from the line
-				if (nextLine != null && nextLine.length > 0) {
-					for (String s : nextLine) {
-						out.print(s + ", ");
-					}
-					out.println();
-				}
+			String nextLine;
+			while ((nextLine = in.readLine()) != null) {
+						out.println(nextLine + ", ");
 			}
 
-			reader.close();
 
 			// adding ticker table
 			Connection result = null;
