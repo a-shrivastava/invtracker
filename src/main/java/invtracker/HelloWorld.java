72bdb4494b9e0a7a98484bff17123cc6e7f5dc08
abhishek <abhishek@192.168.8.107>
2016-01-23 23:25:52 -0500

    added UI for tickers

--- src/main/java/invtracker/HelloWorld.java
+++ src/main/java/invtracker/HelloWorld.java
@@ -16,21 +16,9 @@ public class HelloWorld extends HttpServlet {
       HttpServletResponse response) throws ServletException, IOException 
   {
     // reading the user input
-    String color= request.getParameter("color");    
+    String ticker= request.getParameter("ticker");    
     PrintWriter out = response.getWriter();
-    out.println (
-      "<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\"\"http://www.w3.org/TR/html4/loose.dtd\">\n" +"<html> \n" +
-        "<head> \n" + "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=ISO-8859-1\"> \n" +
-          "<title> My first jsp  </title> \n" +
-        "</head> \n" +
-        "<body> \n" +
-          "<font size=\"12px\" color=\"" + color + "\">" +
-            "Hello World" +
-          "</font> \n" +
-        "</body> \n" +
-      "</html>" 
-    );  
-    
+    out.println(ticker);
     Connection result = null;
     try {
         Context initialContext = new InitialContext();
