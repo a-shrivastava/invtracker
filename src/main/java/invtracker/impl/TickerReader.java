e49fa8d7b1bb4bb911ba5ddb25f65d76aa9769d6
abhishek <abhishek@192.168.8.107>
2016-01-30 14:52:45 -0500

    accounted for business days in data pull

--- src/main/java/invtracker/impl/TickerReader.java
+++ src/main/java/invtracker/impl/TickerReader.java
@@ -26,7 +26,11 @@ public class TickerReader {
 	 */
 	public List<TickerData> read(String ticker, int windowSize) {
 		try {
-			// getting date for 2*windowsSize (e.g. 2*100) days
+			// getting date for 2*windowsSize (e.g. 2*100) days (business days)
+			//in order account for weekends multiply by 1.4; 
+			//to account for holidays and boundary problems add 50*windowSize/100
+			//thus we need data for 1.4*2*windowsize+ceil(50*windowSize/100)
+			
 			DateTime todaysDate = new DateTime(System.currentTimeMillis());
 			int currentMonth = todaysDate.getMonthOfYear() - 1;
 			String currentMonthString = (currentMonth < 10) ? "0"
@@ -38,7 +42,9 @@ public class TickerReader {
 
 			// Suppose windowSize = x, then xDaysBeforeDate is as
 			// hundredDaysBeforeDate
-			DateTime xDaysBeforeDate = todaysDate.minusDays(2 * windowSize + 2);
+			int daysBefore = (int) Math.ceil(1.4*2*windowSize+50*windowSize/100);
+			System.out.println("cal days of data to pull:"+daysBefore);
+			DateTime xDaysBeforeDate = todaysDate.minusDays(daysBefore);
 			int xDaysBeforeMonth = xDaysBeforeDate.getMonthOfYear() - 1;
 			String xDaysBeforeMonthString = (xDaysBeforeMonth < 10) ? "0"
 					+ xDaysBeforeMonth : "" + xDaysBeforeMonth;
